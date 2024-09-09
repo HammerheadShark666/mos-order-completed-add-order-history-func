851e0c0d333dc3723aca7cc07af9ffb4fbffce16
John Miller <john.miller@unionstreet.co.uk>
2024-09-09 17:00:22 +0100

    Service bus client add default credentials

--- Microservice.Order.History.Function/Helpers/AzureServiceBusHelper.cs
+++ Microservice.Order.History.Function/Helpers/AzureServiceBusHelper.cs
@@ -1,4 +1,5 @@
-﻿using Azure.Messaging.ServiceBus;
+﻿using Azure.Identity;
+using Azure.Messaging.ServiceBus;
 using Microservice.Order.History.Function.Helpers.Interfaces;
 
 namespace Microservice.Order.History.Function.Helpers;
@@ -7,7 +8,7 @@ public class AzureServiceBusHelper() : IAzureServiceBusHelper
 {
     public async Task SendMessage(string queue, string data)
     {
-        var client = new ServiceBusClient(EnvironmentVariables.AzureServiceBusConnection);
+        var client = new ServiceBusClient(EnvironmentVariables.AzureServiceBusConnection, new DefaultAzureCredential());
         var sender = client.CreateSender(queue);
 
         await sender.SendMessageAsync(new ServiceBusMessage(data));
