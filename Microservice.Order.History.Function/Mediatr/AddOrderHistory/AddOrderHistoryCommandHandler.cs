aa9250d88b5759153670f7a03042fac19605b97b
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-11 19:10:06 +0100

    Put service bus code back in

--- Microservice.Order.History.Function/Mediatr/AddOrderHistory/AddOrderHistoryCommandHandler.cs
+++ Microservice.Order.History.Function/Mediatr/AddOrderHistory/AddOrderHistoryCommandHandler.cs
@@ -2,12 +2,15 @@
 using MediatR;
 using Microservice.Order.History.Function.Data.Repository.Interfaces;
 using Microservice.Order.History.Function.Domain;
+using Microservice.Order.History.Function.Helpers;
+using Microservice.Order.History.Function.Helpers.Interfaces;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
 
 namespace Microservice.Order.History.Function.MediatR.AddOrderHistory;
 
 public class AddOrderHistoryCommandHandler(IOrderHistoryRepository orderHistoryRepository,
-                                    //IAzureServiceBusHelper azureServiceBusHelper,
+                                    IAzureServiceBusHelper azureServiceBusHelper,
                                     IMapper mapper,
                                     ILogger<AddOrderHistoryCommandHandler> logger) : IRequestHandler<AddOrderHistoryRequest, AddOrderHistoryResponse>
 {
@@ -22,7 +25,7 @@ public class AddOrderHistoryCommandHandler(IOrderHistoryRepository orderHistoryR
             UpdateOrderHistoryItems(orderHistory);
 
             await SaveOrderHistoryAsync(orderHistory);
-            //await SendOrderHistoryAddedToServiceBusQueueAsync(orderHistory.Id);
+            await SendOrderHistoryAddedToServiceBusQueueAsync(orderHistory.Id);
         }
         else
         {
@@ -45,13 +48,13 @@ public class AddOrderHistoryCommandHandler(IOrderHistoryRepository orderHistoryR
         }
     }
 
-    //private async Task SendOrderHistoryAddedToServiceBusQueueAsync(Guid id)
-    //{
-    //    await azureServiceBusHelper.SendMessage(EnvironmentVariables.AzureServiceBusQueueOrderHistoryAdded, GetSerializedOrder(id));
-    //}
+    private async Task SendOrderHistoryAddedToServiceBusQueueAsync(Guid id)
+    {
+        await azureServiceBusHelper.SendMessage(EnvironmentVariables.AzureServiceBusQueueOrderHistoryAdded, GetSerializedOrder(id));
+    }
 
-    //private static string GetSerializedOrder(Guid orderId)
-    //{
-    //    return JsonSerializer.Serialize(new Order(orderId));
-    //}
+    private static string GetSerializedOrder(Guid orderId)
+    {
+        return JsonSerializer.Serialize(new Order(orderId));
+    }
 }
