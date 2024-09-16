2ba8666ead302c87a78e6e9225acfa5a6e7f9836
HammerheadShark666 <103519731+HammerheadShark666@users.noreply.github.com>
2024-09-16 12:22:47 +0100

    Set up Sql Server managed id

--- Microservice.Order.History.Function/Helpers/Constants.cs
+++ Microservice.Order.History.Function/Helpers/Constants.cs
@@ -10,5 +10,11 @@ public class Constants
     public const string FailureReasonValidation = "Validation Errors.";
     public const string FailureReasonInternal = "Internal Error.";
 
-    public const string DatabaseConnectionString = "SQLAZURECONNSTR_ORDER_HISTORY";
+    public const string AzureUserAssignedManagedIdentityClientId = "AZURE_USER_ASSIGNED_MANAGED_IDENTITY_CLIENT_ID";
+    public const string AzureDatabaseConnectionString = "AZURE_MANAGED_IDENTITY_SQL_CONNECTION";
+
+    public const string AzureLocalDevelopmentClientId = "AZURE_LOCAL_DEVELOPMENT_CLIENT_ID";
+    public const string AzureLocalDevelopmentClientSecret = "AZURE_LOCAL_DEVELOPMENT_CLIENT_SECRET";
+    public const string AzureLocalDevelopmentTenantId = "AZURE_LOCAL_DEVELOPMENT_TENANT_ID";
+    public const string LocalDatabaseConnectionString = "LOCAL_CONNECTION";
 }
